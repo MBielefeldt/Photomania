a0653a1deb00c5a0bd761ce6d6e9053699173f65
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2013-06-30 11:45:12 +0200

    ...

--- Photomania/Photo+Flickr.m
+++ Photomania/Photo+Flickr.m
@@ -15,14 +15,30 @@
 - (Photo *)photoWithFlickrInfo:(NSDictionary *)photoDictionary InManagedObjectContext:(NSManagedObjectContext *)context
 {
     Photo *photo = nil;
+
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Photo"];
+    request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"title" ascending:YES]];
+    request.predicate = [NSPredicate predicateWithFormat:@"unique = %@", [[photoDictionary[FLICKR_PHOTO_ID] description]];
     
-    photo = [NSEntityDescription insertNewObjectForEntityForName:@"Photo" inManagedObjectContext:context];
-    
-    photo.title = [photoDictionary[FLICKR_PHOTO_TITLE] description];
-    photo.subtitle = [[photoDictionary valueForKeyPath:FLICKR_PHOTO_DESCRIPTION] description];
-    photo.imageURL = [[FlickrFetcher urlForPhoto:photoDictionary format:FlickrPhotoFormatLarge] absoluteString];
-    
-    
+    NSError *error = nil;
+    NSArray *matches = [context executeFetchRequest:request error:&error];
+                         
+    if (matches == nil || matches.count > 1) {
+        // handle error
+    }
+    else if (matches.count == 0) {
+        photo = [NSEntityDescription insertNewObjectForEntityForName:@"Photo" inManagedObjectContext:context];
+        
+        photo.unique = [photoDictionary[FLICKR_PHOTO_ID] description];
+        photo.title = [photoDictionary[FLICKR_PHOTO_TITLE] description];
+        photo.subtitle = [[photoDictionary valueForKeyPath:FLICKR_PHOTO_DESCRIPTION] description];
+        photo.imageURL = [[FlickrFetcher urlForPhoto:photoDictionary format:FlickrPhotoFormatLarge] absoluteString];
+    }
+    else
+    {
+        photo = [matches lastObject];
+    }
+                         
     return photo;
 }
 
